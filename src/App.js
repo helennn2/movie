96dd1ef64a89a6bcd206a1db3eba54718614fdeb
hyeri <parkri1240@gmail.com>
2022-09-06 11:50:44 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import './App.scss';
+
 import React from 'react';
-import axios from 'axios';
-import Movies from './Movie';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  } //root 위치
-  getMovies = async () => {
-    const {
-      data: {
-        data: {movies},
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    //console.log(movies)
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return(
-      <section className='container'>
-        {isLoading ? (<div className='loader'>
-          <span className='loader_text'>'Loading...'</span>
-          </div>) :(
-            <div className='movies'>
-              {movies.map((movie) => (
-                  <Movies key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} /> 
-              ))}
-            </div>
-          )
-        }
-      </section>
-    );
-  }
+
+function App() {
+  return (
+    <div className='App'>
+        <BrowserRouter>
+            <Navigation />
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/about' element={<About />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </div>
+  )
 }
-
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import axios from 'axios';
+import Movies from '../components/Movie';
+import './Home.scss';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  } //root 위치
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    //console.log(movies)
+    this.setState({movies, isLoading: false})
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return(
+      <section className='container'>
+        {isLoading ? (<div className='loader'>
+          <span className='loader_text'>Loading...</span>
+          </div>) :(
+            <div className='movies'>
+              {movies.map((movie) => (
+                  <Movies key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} /> 
+              ))}
+            </div>
+          )
+        }
+      </section>
+    );
+  }
+}
+
+export default Home;
